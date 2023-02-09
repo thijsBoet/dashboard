cea3fa2aa87969f8c63ce3239f3cbf107289883f
thijsBoet <m.boet@chello.nl>
2023-02-09 01:40:20 +0100

    created all components and pages including index exports

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 const App = () => {
+	const activeMenu = true;
+
 	return (
 		<div>
 			<BrowserRouter>
@@ -14,17 +16,62 @@ const App = () => {
 						<TooltipComponent content='Settings' position='Top'>
 							<button
 								type='button'
-                className='text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray'
-                style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                  borderRadius: '50%',
-                }}
+								className='text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray'
+								style={{
+									backgroundColor: 'rgba(0, 0, 0, 0.5)',
+									borderRadius: '50%',
+								}}
 							>
 								<FiSettings />
 							</button>
 						</TooltipComponent>
 					</div>
-        </div>
+					{activeMenu ? (
+						<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+							Sidebar
+						</div>
+					) : (
+						<div className='w-0 dark:bg-secondary-dark-bg'>Sidebar</div>
+					)}
+					<div
+						className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+							activeMenu ? 'md:ml-72' : 'flex-2'
+						}`}
+					>
+						<div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+							Navbar
+						</div>
+					</div>
+
+					<div>
+						<Routes>
+							{/* Dashboard */}
+							<Route path='/' element='ECommerce' />
+							<Route path='/ecommerce' element='ECommerce' />
+
+							{/* Pages */}
+							<Route path='/orders' element='Orders' />
+							<Route path='/employees' element='employees' />
+							<Route path='/customers' element='customers' />
+
+							{/* Apps */}
+							<Route path='/kanban' element='Kanban' />
+							<Route path='/editor' element='Editor' />
+							<Route path='/calendar' element='Calendar' />
+							<Route path='/color-picker' element='ColorPicker' />
+
+							{/* Charts */}
+							<Route path='/line' element='Line' />
+							<Route path='/area' element='Area' />
+							<Route path='/bar' element='Bar' />
+							<Route path='/pie' element='Pie' />
+							<Route path='/financial' element='Financial' />
+							<Route path='/color-mapping' element='ColorMapping' />
+							<Route path='/pyramid' element='Pyramid' />
+							<Route path='/stacked' element='Stacked' />
+						</Routes>
+					</div>
+				</div>
 			</BrowserRouter>
 		</div>
 	);
