a603f149779e3b9b21ead6e032d56848e9afbdaf
thijsBoet <m.boet@chello.nl>
2023-02-10 13:26:28 +0100

    finished calender

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {
+	ScheduleComponent,
+	ViewsDirective,
+	ViewDirective,
+	Day,
+	Week,
+	WorkWeek,
+	Month,
+	Agenda,
+	Inject,
+	Resize,
+	DragAndDrop,
+} from '@syncfusion/ej2-react-schedule';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
 
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
+	return (
+		<div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+			<Header title='Calendar' category='Page' />
+			<ScheduleComponent
+				height='650px'
+				selectedDate={new Date(2021, 0, 10)}
+				eventSettings={{ dataSource: scheduleData }}
+			>
+				<ViewsDirective>
+					{['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map(item => (
+						<ViewDirective key={item} option={item} />
+					))}
+				</ViewsDirective>
+				<Inject
+					services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+				/>
+			</ScheduleComponent>
+		</div>
+	);
+};
 
-export default Calendar
+export default Calendar;
