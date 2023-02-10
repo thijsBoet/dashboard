11e9f2e031613e58fd64921b0e60bd243b31cce6
thijsBoet <m.boet@chello.nl>
2023-02-10 12:42:04 +0100

    finished employees page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -18,20 +18,31 @@ import { Header } from '../components';
 
 const Orders = () => {
 	return (
-		<div className='m-2 md:m-10 p-2 md:p-10 bg-whote rounded-3xl'>
+		<div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
 			<Header title='Orders' category='Page' />
-      <GridComponent
-        id="gridcomp"
-        dataSource={ordersData}
-        allowPaging
-        allowSorting
-      >
-					<ColumnsDirective>
-						{ordersGrid.map((item, index) => (
-							<ColumnDirective key={index} {...item} />
-						))}
-        </ColumnsDirective>
-        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit]} />
+			<GridComponent
+				id='gridcomp'
+				dataSource={ordersData}
+				allowPaging
+				allowSorting
+			>
+				<ColumnsDirective>
+					{ordersGrid.map((item, index) => (
+						<ColumnDirective key={index} {...item} />
+					))}
+				</ColumnsDirective>
+				<Inject
+					services={[
+						Resize,
+						Sort,
+						ContextMenu,
+						Filter,
+						Page,
+						ExcelExport,
+						PdfExport,
+						Edit,
+					]}
+				/>
 			</GridComponent>
 		</div>
 	);
